b90ef3c86eaa9825491b758bd9a2a1127daba024
tyri0n11 <chauthinh00710@gmail.com>
2023-12-18 12:06:04 +0700

    very first touch

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,11 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("2D Adventure");
+        window.setTitle("Our Adventure");
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         window.pack();
-        window.setLocationRelativeTo(null);x`
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
         gamePanel.setupGame();
         gamePanel.startGameThread();
