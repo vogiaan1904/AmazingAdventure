cac5886267f5624254ba3dac74e0cfca38bc7cd5
tyri0n11 <chauthinh00710@gmail.com>
2023-12-21 14:46:01 +0700

    add dialogue (Oldman) to UI

--- src/entity/NPC_OldMan.java
+++ src/entity/NPC_OldMan.java
@@ -14,7 +14,18 @@ public class NPC_OldMan extends Entity{
         direction ="down";
         speed =1;
         getImage();
+        setDialogue();
     }
+    public void setDialogue(){
+        dialogues[0] = "Hi, adventurer";
+        dialogues[1] = "I'm your father";
+        dialogues[2] = "Hey son!!";
+        dialogues[3] = "I'm Jeda";
+    }
+    public void speak(){
+        gp.ui.currentDialogue = dialogues[dialogueIndex];
+    }
+
     public void getImage(){
         up1 = setup("/npc/oldman_up_1");
         up2 = setup("/npc/oldman_up_2");
@@ -25,6 +36,7 @@ public class NPC_OldMan extends Entity{
         right1 = setup("/npc/oldman_right_1");
         right2= setup("/npc/oldman_right_2");
     }
+
     public void setAction(){
         actionLockCounter++;
         if(actionLockCounter == 120){ // lock for 120 frames / 2s
@@ -46,4 +58,5 @@ public class NPC_OldMan extends Entity{
             actionLockCounter = 0;
         }
     }
+
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -103,6 +103,7 @@ public class Player extends Entity{
     public void interactNPC(int i){
         if(i!=999){
             gp.gameState = gp.dialogueState;
+            gp.npc[i].speak();
         }
     }
     public void draw(Graphics2D g2){ // draw the movement of player
